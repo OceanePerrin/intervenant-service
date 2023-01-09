1a1d9a5db06823ac22174f7922644cd51abcf37e
Olivier Perrin <olivier.perrin@loria.fr>
2023-01-09 10:59:01 +0100

    correction HATEOAS

---
+++ src/main/java/org/miage/intervenantservice/control/IntervenantAssembler.java
@@ -0,0 +1,34 @@
+package org.miage.intervenantservice.control;
+
+import java.util.List;
+import org.miage.intervenantservice.boundary.IntervenantRepresentation;
+import org.miage.intervenantservice.entity.Intervenant;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import java.util.stream.StreamSupport;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@Component
+public class IntervenantAssembler implements RepresentationModelAssembler<Intervenant, EntityModel<Intervenant>> {
+
+    @Override
+    public EntityModel<Intervenant> toModel(Intervenant intervenant) {
+        return EntityModel.of(intervenant,
+                linkTo(methodOn(IntervenantRepresentation.class).getIntervenantById(intervenant.getId())).withSelfRel(),
+                linkTo(methodOn(IntervenantRepresentation.class).getAllIntervenants()).withRel("collection"));
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Intervenant>> toCollectionModel(Iterable<? extends Intervenant> entities) {
+        List<EntityModel<Intervenant>> intervenanModel = StreamSupport
+            .stream(entities.spliterator(), false)
+            .map(this::toModel)
+            .toList();
+
+        return CollectionModel.of(intervenanModel, linkTo(methodOn(IntervenantRepresentation.class)
+            .getAllIntervenants()).withSelfRel());
+    }
+}
